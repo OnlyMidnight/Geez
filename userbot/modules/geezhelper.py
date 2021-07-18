be6d22a706449a9eaa4b54450163ede51073a862
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-18 22:30:00 +0700

    Create geezhelper.py

--- userbot/modules/geezhelper.py
+++ userbot/modules/geezhelper.py
@@ -8,29 +8,28 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.remixhelp$")
+@register(outgoing=True, pattern="^.geezhelp$")
 async def usit(e):
     await e.edit(
         f"Here's something for {DEFAULTUSER} to use it for help_on_update on **XBot-Remix**:\n"
         "\n[Windows Method](https://telegra.ph/How-to-keep-repo-updated-while-keeping-your-changes-through-windows-cmd-method-04-01)"
-        "\n[Termux Method](https://telegra.ph/How-to-keep-XBot-Remix-repo-updated-while-keeping-your-changes-through-Termux--kali-linux-06-02)"
         "\n[Kali Linux Method](https://telegra.ph/How-to-keep-OpenUserBot-repo-updated-while-keeping-your-changes-through-Termux-method-04-01)"
         "\n[Ubuntu Linux Method](https://telegra.ph/How-to-keep-OUB-repo-updated-while-keeping-your-changes-through-Ubuntu-Terminal-method-04-01-2)"
         "\n[Gdrive Tutorial](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[video-tutorial](https://youtu.be/us1O-AnWmHA)")
 
 
-@register(outgoing=True, pattern="^.remixvar$")
+@register(outgoing=True, pattern="^.geezvar$")
 async def var(m):
     await m.edit(
-        f"Here's a list of VARS for {DEFAULTUSER} on **XBot-Remix**:\n"
-        "\n[HEROKU VARS](https://raw.githubusercontent.com/ximfine/XBot-Remix/x-sql-extended/varshelper.txt)")
+        f"Here's a list of VARS for {DEFAULTUSER} on **Geez - UserBot**:\n"
+        "\n[HEROKU VARS](https://raw.githubusercontent.com/vckyou/Geez-UserBot/varshelper.txt)")
 
 
 CMD_HELP.update({
-    "remixhelper":
-    "`.remixhelp`\
+    "geezhelper":
+    "`.geezhelp`\
 \nUsage: Provide links to update repo guides while you keep your changes on the floor.\
-\n`.remixvar`\
+\n`.geezvar`\
 \nUsage: Provide vars to cross check for you."
 })
