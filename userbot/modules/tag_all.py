8ad23da95d3a8cbb77d9655ccc12a426722654f7
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-18 22:48:56 +0700

    Tol

--- userbot/modules/tag_all.py
+++ userbot/modules/tag_all.py
@@ -1,21 +1,22 @@
-import re
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""A Plugin to tagall in the chat for @UniBorg and cmd is `.all`"""
+
+
 from userbot import CMD_HELP, bot
 from userbot.events import register
 
-usernexp = re.compile(r"@(\w{3,32})\[(.+?)\]")
-nameexp = re.compile(r"\[([\w\S]+)\]\(tg://user\?id=(\d+)\)\[(.+?)\]")
 
-
-@register(outgoing=True, pattern="^.all(?: |$)(.*)", disable_errors=True)
+@register(outgoing=True, pattern="^.all$")
 async def all(event):
     if event.fwd_from:
         return
     await event.delete()
-    query = event.pattern_match.group(1)
-    mentions = f"@all {query}"
+    mentions = "@all"
     chat = await event.get_input_chat()
-    async for x in bot.iter_participants(chat, 10000):
-        mentions += f"[\u2063](tg://user?id={x.id} {query})"
+    async for x in bot.iter_participants(chat, 200000):
+        mentions += f"[\u2063](tg://user?id={x.id})"
     await bot.send_message(chat, mentions, reply_to=event.message.reply_to_msg_id)
 
 
