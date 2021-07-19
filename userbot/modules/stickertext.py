57591953acb264ba762ed5da7fdee8ba868765ec
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-19 14:30:31 +0700

    Update stickertext.py

--- userbot/modules/stickertext.py
+++ userbot/modules/stickertext.py
@@ -30,15 +30,15 @@ async def stext(event):
 
     image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
     draw = ImageDraw.Draw(image)
-    fontsize = 220
+    fontsize = 210
     font = ImageFont.truetype(
-        "userbot/files/RobotoMono-Regular.ttf",
+        "userbot/resources/fonts/FontGeez.otf",
         size=fontsize)
 
     while draw.multiline_textsize(sticktext, font=font) > (512, 512):
         fontsize -= 3
         font = ImageFont.truetype(
-            "userbot/files/RobotoMono-Regular.ttf",
+            "userbot/resources/fonts/FontGeez.otf",
             size=fontsize)
 
     width, height = draw.multiline_textsize(sticktext, font=font)
